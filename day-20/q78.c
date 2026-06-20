0799200297e0b9fd189449ac57420390e8a7cc8e
nishantkumar-GLB <nishantkumar05582@gmail.com>
2026-06-20 21:19:51 +0530

    day-20 uploaded

---
+++ day-20/q78.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n, isSymmetric = 1;
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+
+    int a[10][10];
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] != a[j][i]) {
+                isSymmetric = 0;
+                break;
+            }
+        }
+    }
+
+    if (isSymmetric) {
+        printf("The matrix is symmetric.\n");
+    } else {
+        printf("The matrix is not symmetric.\n");
+    }
+
+    return 0;
+}
